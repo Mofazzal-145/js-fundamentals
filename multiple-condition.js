1393e8dede69cd7f2c2675be9b4519c2911d2953
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-01-27 03:58:25 +0600

    multiple-condition file added

---
+++ multiple-condition.js
@@ -0,0 +1,8 @@
+var gotJob = true;
+var moneySaved = 150000;
+if(gotJob == true && moneySaved >2000000 ){
+    console.log('i will buy a car');
+}
+else{
+    console.log('tor kopala a dukko asa');
+}
