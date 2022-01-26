57007cab0fc59f9b65d62fd8c1e1122a3f6d5663
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-01-27 01:50:08 +0600

    array-index file added

---
+++ array-index.js
@@ -0,0 +1,13 @@
+var books = ['first', 'second', 'third','fourth','fifth'];
+var bookIndex = books.indexOf('third');
+// console.log(bookIndex);
+var numbers = [22,33,44,66,55,77];
+var index = numbers.indexOf(77);
+console.log(numbers);
+numbers [1] =101;
+numbers [3] =99;
+console.log(numbers);
+
+// console.log(index);
+var secondIndex = books[2];
+// console.log(secondIndex);
