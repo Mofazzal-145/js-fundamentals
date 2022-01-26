344687ae593d58db203e9e0c327e8fc8aa836bfb
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-01-27 02:22:40 +0600

    push-pop file added

---
+++ push-pop.js
@@ -0,0 +1,25 @@
+// add element from an array 
+var lastBench = ['mofu', 'mofazzal', 'shakil'];
+lastBench.push('shakib');
+lastBench.push('mamun');
+// console.log(lastBench);
+var friendsAge = [11,13,19, 17,22];
+// friendsAge.push(15);
+// console.log(friendsAge);
+
+// remove Element from an Array
+friendsAge.pop();
+// console.log(friendsAge);
+var lastItem = friendsAge.pop()
+// console.log(lastItem);
+
+// remove first element of an array 
+let cats = ['Bob', 'Willy', 'Mini'];
+// cats.shift();
+// console.log(cats);
+
+// add Element at the first of an array 
+let cats2 = ['mew'];
+cats2.unshift('kalsfdj'); 
+cats2.unshift('Puff', 'George');
+console.log(cats2);
