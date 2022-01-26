1ea86d6221ba64aade4bc67488d23ed4da002974
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-01-27 01:05:02 +0600

    array file added

---
+++ array.js
@@ -0,0 +1,11 @@
+// Array
+var friendsAge = [11, 13, 15, 17];
+
+var schoolFee = [200, 322, 444];
+var names = ['mofu','mofazzal','mohitul'];
+var oddNumbers = [1,3,5,7,9];
+var vowels = ['a','e','i','o','u'];
+var vowelsCount = vowels.length;
+
+// console.log(friendsAge);
+console.log(vowelsCount);
