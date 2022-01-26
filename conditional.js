991ae51852f7c83471e8b29b840fe866d87d4f31
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-01-27 03:40:39 +0600

    conditional file added

---
+++ conditional.js
@@ -0,0 +1,21 @@
+var foodReady = true;
+if(foodReady==true){
+    // console.log('mama!! i will eat rice');
+}
+var iphonePrice = 70000;
+var myBudget = 10000;
+// if(iphonePrice < myBudget){
+//     console.log('I will buy it');
+// }
+// if(iphonePrice > myBudget){
+//     console.log('Shawmi is the best');
+// }
+
+var chickenPrice = 100;
+var myCash = 50;
+if(chickenPrice < myCash){
+    console.log("I will eat");
+}
+else{
+    console.log("smashed potato with lentil soup");
+}
